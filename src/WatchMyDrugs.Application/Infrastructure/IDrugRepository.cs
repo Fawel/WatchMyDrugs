1aaff59deb0ae9d76d94c716489d86ffde409c3e
Fawel <uiasop@gmail.com>
2020-07-21 02:50:22 +0300

    Комментик для репы

--- src/WatchMyDrugs.Application/Infrastructure/IDrugRepository.cs
+++ src/WatchMyDrugs.Application/Infrastructure/IDrugRepository.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 
 namespace WatchMyDrugs.Application.Infrastructure
 {
+    // Репозиторий для получения инфы про лекарства и их типы
     public interface IDrugRepository
     {
         Task GetDrugTypesAsync(CancellationToken token = default);
